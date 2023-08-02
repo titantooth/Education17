667b3f3a03147253578019f6a619e15b67282ac7
titantooth <ali.rabieeie@gmail.com>
2023-08-02 14:30:04 +0330

    s

--- js/behave.js
+++
@@ -1,7 +0,0 @@
-let menuButton = document.querySelector("#menu-btn");
-let navbar = document.querySelector(".header .flex .navbar");
-
-menuButton.onclick = () => {
-  menuButton.classList.toggle("fa-times");
-  navbar.classList.toggle("active");
-};
